879ad71628e0c10d129d53bff5bd19b0aa2ae643
Hsiang Chou <hsiangchou@eaton.com>
2025-10-15 11:54:26 +0100

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ backgroundColor: 'blue', height: '100vh' }}>
+      <h1 style={{ color: 'white', textAlign: 'center', paddingTop: '40vh' }}>
+        Hello Copilot Agent!
+      </h1>
+    </div>
+  );
+}
